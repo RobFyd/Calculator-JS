19316194434606ff93bd5599c4e1ffdd9799a048
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2023-03-25 21:53:42 +0000

    querySelectors added

---
+++ script.js
@@ -0,0 +1,17 @@
+const currentNumber = document.querySelector(".js-currentNumber");
+
+const previousNumber = document.querySelector(".js-previousNumber");
+
+const mathSign = document.querySelector(".js-mathSign");
+
+const numbersButtons = document.querySelectorAll(".js-number");
+
+const operatorsButtons = document.querySelectorAll(".js-operator");
+
+const equalsButtons = document.querySelector(".js-js-equals");
+
+const clearButton = document.querySelector(".js-clear");
+
+const calculatorHistory = document.querySelector(".js-history");
+
+const historyBtn = document.querySelector(".js-historyBtn");
