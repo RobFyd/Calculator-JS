b5a0bf349aaaeb1aff58bfbcae2f3336e3923fe3
Robert Fydrych <123332510+RobFyd@users.noreply.github.com>
2024-04-10 00:22:16 +0100

    assertions added

--- script.ts
+++ script.ts
@@ -1,11 +1,11 @@
 console.log('Hello, world!');
 
-const currentNumber = document.querySelector(".js-currentNumber");
-const previousNumber = document.querySelector(".js-previousNumber");
-const mathSign = document.querySelector(".js-mathSign");
-const numbersButtons = document.querySelectorAll(".js-number");
-const operatorsButtons = document.querySelectorAll(".js-operator");
-const equalsButton = document.querySelector(".js-equals");
-const clearButton = document.querySelector(".js-clear");
-const calculatorHistory = document.querySelector(".js-history");
-const historyBtn = document.querySelector(".js-historyBtn");
+const currentNumber = document.querySelector(".js-currentNumber")as HTMLParagraphElement;
+const previousNumber = document.querySelector(".js-previousNumber")as HTMLParagraphElement;
+const mathSign = document.querySelector(".js-mathSign")as HTMLSpanElement;
+const numbersButtons = document.querySelectorAll(".js-number")as NodeListOf<HTMLButtonElement>;
+const operatorsButtons = document.querySelectorAll(".js-operator")as NodeListOf<HTMLButtonElement>;
+const equalsButton = document.querySelector(".js-equals")as HTMLButtonElement;
+const clearButton = document.querySelector(".js-clear")as HTMLButtonElement;
+const calculatorHistory = document.querySelector(".js-history")as HTMLDivElement;
+const historyBtn = document.querySelector(".js-historyBtn")as HTMLButtonElement;
